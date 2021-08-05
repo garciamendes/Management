9c46e4c59c82e693df633471bb87b2462c111801
garciamendes <garciaprog0101@gmail.com>
2021-08-05 18:45:38 -0300

    Basic login screen completed

--- frontend/src/containers/Auth/login/index.tsx
+++ frontend/src/containers/Auth/login/index.tsx
@@ -1,14 +1,77 @@
 // React
-import React from 'react'
+import React, { ReactNode, useState } from 'react'
+
+// Local
+import DemoImg from '../../../static/Images/log-in.svg'
+import Logo from '../../../static/Images/logo.svg'
+import EyeON from '../../../static/Images/eye-on.svg'
+import EyeOff from '../../../static/Images/eye-off.svg'
 
 // Styles
-import { Container } from './styles'
-
+import {
+  Container,
+  SideContainer,
+  ContainerImageDemo,
+  Image,
+  Input,
+  Button,
+  ImageEye
+} from './styles'
 
 function Login() {
+  const [showPassword, setShowPassword] = useState(false)
+
+  function handleShowPassword() {
+    setShowPassword(!showPassword)
+  }
+
+  function handleEnter() {
+
+    alert('Funciona')
+  }
+
   return (
     <Container>
-      <h1>Login a</h1>
+      <SideContainer>
+        <div className='container_side_secundary'>
+          <Image
+            size={"30"}
+            src={Logo}
+            alt='Logo'
+          />
+          <form className='form_login_user'>
+            <Input type='text' placeholder='Username' />
+            <div className='content_password_eye'>
+              <Input type={`${showPassword ? 'type' : 'password'}`} placeholder='Password' />
+              {showPassword ? (
+                <ImageEye
+                  size={'10'}
+                  src={EyeOff}
+                  alt='show password'
+                  onClick={handleShowPassword}
+                />
+              ) : (
+                <ImageEye
+                  size={'10'}
+                  src={EyeON}
+                  alt='show password'
+                  onClick={handleShowPassword}
+                />
+              )}
+            </div>
+          </form>
+          <Button onClick={handleEnter}>
+            Enter
+          </Button>
+        </div>
+      </SideContainer>
+      <ContainerImageDemo>
+        <Image
+          size={"45"}
+          src={DemoImg}
+          alt='Image demo'
+        />
+      </ContainerImageDemo>
     </Container>
   );
 };
--- frontend/src/containers/Auth/login/styles.ts
+++ frontend/src/containers/Auth/login/styles.ts
@@ -1,6 +1,97 @@
-import styled from 'styled-components';
+// Third party
+import styled from 'styled-components'
+
+// Const
+import { COLORS } from '../../../styles/variables'
+
+interface ImageProps {
+  size: string,
+}
 
 export const Container = styled.div`
   display: flex;
   height: 100%;
-`;
+`
+
+export const SideContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  width: 40%;
+  background: ${COLORS.White};
+  box-shadow: 1px 0px 20px rgba(0, 0, 0, 0.12);
+  border-radius: 0px 50px 50px 0px;
+
+  .container_side_secundary {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    gap: 7px;
+    max-width: 50%;
+  }
+
+  .form_login_user {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+    height: 40%;
+    width: 100%;
+
+    .content_password_eye {
+      position: relative;
+      width: 100%;
+    }
+  }
+`
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 4% 3%;
+  font-size: 18px;
+  border: none;
+  background: ${COLORS.BackgroundInput};
+  border-radius: 5px;
+`
+
+export const Button = styled.button`
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 4% 0;
+  font-size: 18px;
+  border: none;
+  border-radius: 5px;
+  color: ${COLORS.White};
+  background-color: ${COLORS.GreenLight};
+  transition: .24s;
+
+  &:hover {
+    filter: grayscale(30%);
+  }
+`
+
+export const ContainerImageDemo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 60%;
+`
+
+export const Image = styled.img<ImageProps>`
+  width: ${(props) => props.size}%;
+`
+
+export const ImageEye = styled.img<ImageProps>`
+  cursor: pointer;
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  width: ${(props) => props.size}%;
+`
