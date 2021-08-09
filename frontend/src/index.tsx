c16305aae0b930a5969ce247ef7ac0e793c9bc03
garciamendes <garciaprog0101@gmail.com>
2021-08-09 11:05:25 -0300

    Added utility token and redux connection

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// Redux
+import { Provider } from 'react-redux'
+
+// Third party
+import { Toaster } from 'react-hot-toast'
+import 'semantic-ui-css/semantic.min.css'
+
 // Project
 import App from './App'
+import store from './stores'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+      />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 )
--- frontend/src/services/Api.ts
+++ frontend/src/services/Api.ts
@@ -4,3 +4,5 @@ import Axios from 'axios'
 export const api = Axios.create({
   baseURL: process.env.REACT_APP_API_URL
 })
+
+export const AuthTokenHeader = localStorage.getItem('token')
