943f01f642d4c768efc2ef1d0d668bbc2d418627
garciamendes <garciaprog0101@gmail.com>
2021-08-10 02:59:22 -0300

    root main (rootReducer, rootSaga) add host details

--- frontend/src/stores/modules/rootReducer.ts
+++ frontend/src/stores/modules/rootReducer.ts
@@ -4,10 +4,12 @@ import { combineReducers } from 'redux'
 // project
 import auth from './auth/reducers'
 import hosts from './assets/reducers'
+import hostDetail from './detailHost/reducers'
 import vulnerabilities from './vulnerability/reducers'
 
 export default combineReducers({
   auth,
   hosts,
+  hostDetail,
   vulnerabilities
 })
